234b176bfe78da8be09be74658a012c8beb2c0a8
zhangzhe <760583490@qq.com>
2023-02-08 11:51:42 +0800

    Initial Commit.

---
+++ src/main/java/com/zhang/demos/SpringDemosApplication.java
@@ -0,0 +1,19 @@
+package com.zhang.demos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序启动类
+ *
+ * @author zhangzhe
+ * @date 2023-02-08 11:49
+ */
+@SpringBootApplication
+public class SpringDemosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemosApplication.class, args);
+    }
+
+}
